95651a025cd000813c2a47e90746e3c1ace57c5c
alber <alber@DESKTOP-8AM6JBC>
2022-04-01 17:22:09 +0200

    prueba

---
+++ PrAgenda/src/contactos/Ejemplo_git.java
@@ -0,0 +1,10 @@
+package contactos;
+
+public class Ejemplo_git {
+
+	public static void main(String[] args) {
+		System.out.print("En este proyecto trabajamos con el repositorio GITHUB");
+
+	}
+
+}
